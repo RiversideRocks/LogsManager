bd89eae0e4f14a9f746b428a9740ce65b85de3cf
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-08-29 17:55:38 -0400

    href

--- lists_download.py
+++ lists_download.py
@@ -4,6 +4,6 @@ from bs4 import BeautifulSoup
 url = "https://web.archive.org/web/20220328132419/https://dataset.domainsproject.org/generic_com/"
 html = requests.get(url, headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
 soup = BeautifulSoup(html.text, "html.parser")
-links = soup.find_all('a').text
+links = soup.find_all('a')
 for link in links:
-    print(link)
+    print(link['href'])
