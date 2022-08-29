935cba693739a2f07567d4214b712ec3a54db5b2
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-08-28 21:27:50 -0400

    try a site with Cloudflare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.tsa.gov"
+url = "https://www.fbi.gov"
 html = requests.get(url, headers={"User-agent": "Master 1337 Hacker"})
 soup = BeautifulSoup(html.text, "html.parser")
 print(soup.title)
