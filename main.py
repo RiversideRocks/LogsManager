2c716f6ba0befe70aec62ae5e9822e6b1c75e758
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-08-28 21:22:03 -0400

    bs4 test

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from bs4 import BeautifulSoup
 
 url = "https://www.tsa.gov"
 html = requests.get(url, headers={"User-agent": "Master 1337 Hacker"})
+soup = BeautifulSoup(html.text, "html.parser")
+print(soup.title)
