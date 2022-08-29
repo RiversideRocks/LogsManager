47b7ef3e60b5c1a6b5ba45dc8bd2d8ae19cdbc65
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-08-29 18:25:57 -0400

    clean up

--- lists_download.py
+++ lists_download.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://web.archive.org/web/20220328132419/https://dataset.domainsproject.org/generic_com/"
+url = "https://web.archive.org/web/20220328132419if_/https://dataset.domainsproject.org/generic_com/"
 html = requests.get(url, headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
 soup = BeautifulSoup(html.text, "html.parser")
 links = soup.find_all('a')
 for link in links:
     if ".txt" in link['href']:
-        href = requests.head("https://web.archive.org/web/20220328142042if_/https://dataset.domainsproject.org/generic_com/" + link['href'], headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
-        print(href.status_code)
+        print(link['href'])
+        #href = requests.head("https://web.archive.org/web/20220328142042if_/https://dataset.domainsproject.org/generic_com/" + link['href'], headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
+        #print(href.status_code)
