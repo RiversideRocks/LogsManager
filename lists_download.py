728aec24c51d3bd7d87cfe1fa441401edc26b152
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-08-29 18:22:33 -0400

    more

--- lists_download.py
+++ lists_download.py
@@ -7,4 +7,5 @@ soup = BeautifulSoup(html.text, "html.parser")
 links = soup.find_all('a')
 for link in links:
     if ".txt" in link['href']:
-        print("OK URL: " + link['href'])
+        href = requests.get("https://web.archive.org/web/20220328142042if_/https://dataset.domainsproject.org/generic_com" + link['href'], headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
+        print(href.status_code)
