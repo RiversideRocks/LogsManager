346b3a043381302dbae062ee62e985ac3f9896ec
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-09-02 13:38:35 -0400

    yes

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ for stuff in f:
     if ".txt" in stuff:
         current_file = open('lists/' + stuff, 'r')
         lines = len(current_file.readlines())
-  
+
         c = 0
         ran_lines = current_file.readlines()
         print(lines)
-        for url in current_file: # url is the url, and c is the counter
+        for url in current_file.readlines(): # url is the url, and c is the counter
         #    c = c +1
             print(url)
         #    print("We are " + str(c) + "/" + str(ran_lines))
