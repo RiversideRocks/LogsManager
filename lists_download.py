8ddde6b1445468edf0f32264cef37bea18fe3129
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-09-02 08:55:35 -0400

    update list program

--- lists_download.py
+++ lists_download.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-url = "https://web.archive.org/web/20220328132419if_/https://dataset.domainsproject.org/generic_com/"
+print("Downloading from the archive.org via the Wayback Machine...")
+print("This may take a long time!")
+time.sleep(5)
+
+url = "https://web.archive.org/web/20220328132419/https://dataset.domainsproject.org/generic_com/"
 html = requests.get(url, headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
 soup = BeautifulSoup(html.text, "html.parser")
 links = soup.find_all('a')
 for link in links:
     if ".txt" in link['href']:
-        print(link['href'])
-        #href = requests.head("https://web.archive.org/web/20220328142042if_/https://dataset.domainsproject.org/generic_com/" + link['href'], headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
-        #print(href.status_code)
+        href = requests.get("https://web.archive.org/web/20220328142042if_/https://dataset.domainsproject.org/generic_com/" + link['href'], headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
+        print("GET /" + link['href'] + " - " + href.status_code)
