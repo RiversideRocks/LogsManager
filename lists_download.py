f953d87aa8783b574e800b61c3bc28960a25c031
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-09-02 09:09:01 -0400

    write to the file bro

--- lists_download.py
+++ lists_download.py
@@ -10,7 +10,12 @@ url = "https://web.archive.org/web/20220328132419/https://dataset.domainsproject
 html = requests.get(url, headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
 soup = BeautifulSoup(html.text, "html.parser")
 links = soup.find_all('a')
+file = 0
 for link in links:
     if ".txt" in link['href']:
         href = requests.get("https://web.archive.org/web/20220328142042if_/https://dataset.domainsproject.org/generic_com/" + link['href'], headers={"User-agent": "RiversideRocks (+https://riverside.rocks)"})
         print("GET /" + link['href'] + " - " + str(href.status_code))
+        write_to = open('lists/dataset_' + str(file) + '.txt', 'a')
+        write_to.write(href.text)
+        write_to.close()
+        file = file + 1
