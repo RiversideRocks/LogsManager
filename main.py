96cc156a8f9f25f68911d8dc1673feea8d9f4aa8
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-09-02 09:31:55 -0400

    count those bad boys

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+f = os.listdir('lists')
+for stuff in f:
+    if ".txt" in stuff:
+        current_file = open('files/' + stuff, 'r')
+        lines = current_file.readlines()
+  
+        c = 0
+        ran_lines = current_file.readlines()
+        for url in lines: # url is the url, and c is the counter
+            c = c +1
+            
+        
+            print("We are " + str(c) + "/" + str(ran_lines))
 
 url = "https://www.fbi.gov"
 html = requests.get(url, headers={"User-agent": "Master 1337 Hacker"})
