190515a5ad46593f27003c327ed79023e351a4a9
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-09-02 13:49:30 -0400

    test out DNS checking method

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import dnspython as dns
+import dns.resolver
+import sys
 
 blacklist = ["myvzw.com", "spectrum.com", "comcast.net", "netvigator.com", "reverse-mundo-r.com", "naturalwireless.com"]
+
+result = dns.resolver.query('8f248fhdsmdiinsfindf.com', 'A')
+for ipval in result:
+    print('IP', ipval.to_text())
+sys.exit()
+
+# Note to self: consider skipping the first domain file. It contains mostly ISP domains
+# that tend to include nothing of use
 
 f = os.listdir('lists')
 for stuff in f:
